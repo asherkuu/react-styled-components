da42ea13adc6467d50a0c19007bca520b4f72d46
asherkuu <asher_kuu@naver.com>
2020-11-02 12:43:45 +0900

    d

--- build/server.js
+++ build/server.js
@@ -1,23 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cors = require("cors");
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var cors = require("cors");
 // const cookieParser = require("cookie-parser");
 
-const app = express();
+var app = express();
 app.use(bodyParser.json());
 app.use(cors()); // Cross Origin Resource Sharing
 
 app.use(express.static(path.join(__dirname, "../client/build"))); // build 되어진 데이터를 가져와서 서버에서 뿌려줌
 
-app.get("/api/data", (req, res) => {
-    const json = { result: "true" };
+app.get("/api/data", function (req, res) {
+    var json = { result: "true" };
     res.send(json);
 });
 
 // It must be place below other crud url request
-app.get("*", (req, res) => {
+app.get("*", function (req, res) {
     res.send(express.static(path.join(__dirname, "../client/build/index.html")));
 });
 
 module.exports = app;
+//# sourceMappingURL=server.js.map
