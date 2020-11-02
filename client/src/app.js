ff17c55b66d097e7000e8049809143c7efc49aa9
asherkuu <asher_kuu@naver.com>
2020-11-02 13:12:58 +0900

    e

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
+    useEffect(() => {
+        fetch("/api/data")
+            .then((res) => res.json())
+            .then((res) => console.log(res));
+    });
+
     return (
         <>
             <JumbotronContainer />
