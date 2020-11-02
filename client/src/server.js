c628c8cf995fb19fe0805473ed1462b3cc5f86c4
asherkuu <asher_kuu@naver.com>
2020-11-02 13:04:14 +0900

    d

---
+++ client/src/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 9000;
+app.use(bodyParser.json());
+app.use(cors()); // Cross Origin Resource Sharing
+
+app.get("/", (req, res) => {
+    console.log(`Wellcome to dpg-server..`);
+});
+
+app.get("/api/data", (req, res) => {
+    const json = { result: "true" };
+    res.send(json);
+});
+
+// listen
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+});
